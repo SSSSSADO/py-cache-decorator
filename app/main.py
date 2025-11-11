3ebe4dcc6842f51ac428742121c8b56914f46cc0
SSSSSADO <grushkovik.danil@gmail.com>
2025-11-11 08:19:58 +0300

    ÐBug fixes and optimization

--- app/main.py
+++ app/main.py
@@ -3,12 +3,11 @@ from typing import Callable
 
 def cache(func: Callable) -> Callable:
     saving_cache = {}
-    def wrapper(*args) -> None:
+    def wrapper(*args):
         if args in saving_cache:
             print("Getting from cache")
-            return saving_cache[args]
         else:
             print("Calculating new result")
             saving_cache[args] = func(*args)
-            return saving_cache[args]
+        return saving_cache[args]
     return wrapper
